9d545ce593b559125ef96588d0cdd49fdf7be5b8
pandino126 <re.luca95@gmail.com>
2025-07-12 20:53:59 +0200

    d

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -6,7 +6,7 @@ import '../styles/Footer.css'
 
 const MyFooter = () => {
   return (
-    <footer className="bg-black border-bottom border-body" data-bs-theme="dark w-100 text-white py-4 mt-5">
+    <footer className="bg-black border-bottom border-body" data-bs-theme="dark w-100  py-4 mt-5 gold-text">
       <Container>
         {/* Lingua */}
         <Row className="mb-3 pt-3 mt-1">
@@ -16,24 +16,24 @@ const MyFooter = () => {
         </Row>
 
         {/* Link */}
-        <Row className="mb-2 text-center text-white gy-2">
+        <Row className="mb-2 text-center gy-2 gold-text">
           <Col xs={12} md="auto" className='gold-text'>
             <small>© 2025 Fuori di testo Inc. Tutti i diritti riservati.</small>
           </Col>
         </Row>
 
-        <Row className="text-center gy-2 small text-white">
+        <Row className="text-center gy-2 small gold-text">
           <Col xs={12} md="auto" className='text-white'>
-            <a href="#" className="text-white text-decoration-none">Condizioni dei servizi internet</a>
+            <a href="#" className="gold-text-decoration-none">Condizioni dei servizi internet</a>
           </Col>
           <Col xs={12} md="auto">
-            <a href="#" className="text-white text-decoration-none">Fuori di testo e privacy</a>
+            <a href="#" className="gold-text-decoration-none">Fuori di testo e privacy</a>
           </Col>
           <Col xs={12} md="auto">
-            <a href="#" className="text-white text-decoration-none">Avviso sui cookie</a>
+            <a href="#" className="gold-text-decoration-none">Avviso sui cookie</a>
           </Col>
           <Col xs={12} md="auto">
-            <a href="#" className="text-white text-decoration-none">Supporto</a>
+            <a href="#" className="gold-text-decoration-none">Supporto</a>
           </Col>
           <Col xs={12} md="auto">
            <Nav.Link as={Link} to="/dicono-di-noi">Dicono di noi</Nav.Link>
