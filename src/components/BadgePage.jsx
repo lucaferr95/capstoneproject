28f49615de5eba73ccb1161875274a9c3c26ce62
pandino126 <re.luca95@gmail.com>
2025-08-05 16:56:07 +0200

    Update BadgePage.jsx

--- src/components/BadgePage.jsx
+++ src/components/BadgePage.jsx
@@ -115,10 +115,7 @@ const BadgePage = () => {
         >
           Vai al Quiz
         </button>
-        <button onClick={() => dispatch(setPointsForUser(userId, points + 50))}>
-  Aggiungi 50 punti
-</button>
-
+       
       </div>
     </div>
   );
